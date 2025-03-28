032c96119c18ad628d54742f42dadb7fb4808a14
Marieve-Trottier <marievetrotttier@gmail.com>
2025-03-27 22:43:20 -0400

    Coquille de majuscule oupsi hihi

--- Taxo.R
+++ Taxo.R
@@ -2,7 +2,7 @@ install.packages("ritis")
 
 # Fonction pour récupérer le TSN depuis ITIS
 Code_tsn <- function(Nom_scientifique) {
-  tsn <- ritis::itis_search(nom_scientifique)$tsn
+  tsn <- ritis::itis_search(Nom_scientifique)$tsn
   if (!is.null(tsn) && length(tsn) > 0) {
     return(tsn)
   } else {
