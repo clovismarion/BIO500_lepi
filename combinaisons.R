d4ad23ec6769338bd435f61a8fbfb302c76d7ceb
VanHaiVu <lapinquimangeunebanane@gmail.com>
2025-04-15 11:42:51 -0400

    nouvelle fonction pour distinct ET ajouter une colonne ID

--- combinaisons.R
+++ combinaisons.R
@@ -1,7 +1,16 @@
 library(dplyr)
 
-combinaisons <- function(df){
-  df <- df %>% distinct()
+combinaisons <- function(df, df_name = deparse(substitute(df))) {
   
-  return(df)
+  # Enlève les informations double
+  y <- df %>%
+    distinct()
+  
+  # Génère une colomne ID
+  id_col_name <- paste0(df_name, "_id")
+  
+  # Ajoute des ID dans la colonne ID
+  y[[id_col_name]] <- seq_len(nrow(y))
+  
+  return(y)
 }
