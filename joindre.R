a1c1e1383573d22206445b1f91a9e3529876da92
VanHaiVu <lapinquimangeunebanane@gmail.com>
2025-04-15 11:55:44 -0400

    nouvelle fonction pour joindre les ids au main dataframe

---
+++ joindre.R
@@ -0,0 +1,11 @@
+joindre_id <- function(data, df, df_name = deparse(substitute(df))){
+  
+  colonnes <- c(setdiff(names(df), paste0(df_name, "_id")))
+  
+  data <- data %>%
+    left_join(df, by = colonnes)
+  
+  return(data)
+}
+
+data <- joindre_id(data, site)
