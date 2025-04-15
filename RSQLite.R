fdad8c66f7e2763c85cba63a729f2f6945be06e7
clovismarion <clovis.marion@gmail.com>
2025-04-15 12:31:15 -0400

    corrections et intégrer les id

--- RSQLite.R
+++ RSQLite.R
@@ -11,11 +11,10 @@ con <- dbConnect(SQLite(), dbname="lepidoptere.db")
 #Création de la table observation
 tbl_observation <- "
 CREATE TABLE observation (
-  obs_id            INTEGER NOT NULL,
+  obs_id            INTEGER PRIMARY KEY,
   obs_unit          VARCHAR(40),
   obs_variable      VARCHAR(50),
-  obs_value         VARCHAR(200),
-  PRIMARY KEY (obs_id)
+  obs_value         VARCHAR(200)
 );"
 dbSendQuery(con,tbl_observation)
 
@@ -25,22 +24,21 @@ dbSendQuery(con,tbl_observation)
 
 tbl_info <- "
 CREATE TABLE info (
-  info_id                INTEGER NOT NULL,
+  info_id                INTEGER PRIMARY KEY,
   original_source        VARCHAR(200),     
   creator                VARCHAR(100),
   title                  VARCHAR(100),
   publisher              VARCHAR(200),
   intellectual_rights    VARCHAR(200),
   license                VARCHAR(200),
-  owner                  VARCHAR(100),
-  PRIMARY KEY (info_id)
+  owner                  VARCHAR(100)
 );"
 dbSendQuery(con, tbl_info)
 
 # Création de la table Site
 tbl_site <- "
 CREATE TABLE site (
-  site_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  site_id INTEGER PRIMARY KEY,
   lat     REAL(7),
   lon     REAL(7)
 );"
@@ -50,13 +48,13 @@ dbSendQuery(con, tbl_site)
 
 tbl_main <- "
 CREATE TABLE main (
-  id                            INTEGER PRIMARY KEY AUTOINCREMENT,
+  main_id                       INTEGER PRIMARY KEY,
   observed_scientific_name      VARCHAR(100),
   year_obs                      INTEGER,
   day_obs                       INTEGER,
   time_obs                      TIME,
   dwc_event_date                DATE,
-  Obs_id                        INTEGER,
+  obs_id                        INTEGER,
   info_id                       INTEGER,
   site_id                       INTEGER,
   FOREIGN KEY (obs_id) REFERENCES observation(obs_id)
@@ -67,21 +65,22 @@ dbSendQuery(con, tbl_main)
   
 
 # INJECTION DES DONNÉES
-dbWriteTable(con, "main", main, append = TRUE, row.names = FALSE)
-dbWriteTable(con, "observation", obs, append = TRUE, row.names = FALSE)
-dbWriteTable(con, "info", info, append = TRUE, row.names = FALSE)
-dbWriteTable(con, "site", site, append = TRUE, row.names = FALSE)
+dbWriteTable(con, "main", main, row.names = FALSE, overwrite=TRUE)
+dbWriteTable(con, "observation", obs, row.names = FALSE, overwrite=TRUE)
+dbWriteTable(con, "info", info, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(con, "site", site, overwrite = TRUE, row.names = FALSE)
 
 
 #Requête pour la Q1 de la variation du nombre d'espèce selon la latitude
 requete_Q1 <- "
-SELECT main.observed_scientific_name, site_id
+SELECT main.observed_scientific_name, site.site_id
 FROM main
-INNER JOIN site ON main.site_id = site_id;
+left JOIN site
+  ON site.site_id = site.site_id;
 "
 
-Q1 <- dbGetQuery(con, query)
-View(result)
+Q1 <- dbGetQuery(con, requete_Q1)
+View(Q1)
 
 #Requête pour la Q2 de la variation du nombre d'espèce selon la longitude
 
@@ -105,6 +104,11 @@ left JOIN site
   ON site.site_id = site_id;
 #Where main.Qc is True
 "
+
+requete_Q3 <- "
+SELECT * FROM main LIMIT 5;
+"
+
 Q3 <- dbGetQuery(con, requete_Q3)
 View (Q3)
 
