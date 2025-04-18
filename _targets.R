15ec1cbfeab90ed1b40c9c817f85813c29f4020b
VanHaiVu <lapinquimangeunebanane@gmail.com>
2025-04-18 14:20:44 -0400

    debugg target

--- _targets.R
+++ _targets.R
@@ -5,10 +5,17 @@
 library(targets)
 library(rmarkdown)
 library(tarchetypes)
+library(dplyr)
 tar_option_set(packages = c("MASS", "igraph"))
 
 # Scripts R
-source()
+source("fonctions/clean.R")
+source("fonctions/combinaisons.R")
+source("fonctions/Donnee_Quebec.R")
+source("fonctions/joindre.R")
+source("fonctions/load.R")
+source("fonctions/select_columns.R")
+source("fonctions/valider.R")
 
 # Pipeline
 list(
@@ -21,15 +28,59 @@ list(
     clean(df_list)
   ),   
   tar_target(
-    data, 
+    data_1, 
     validate(donnees_propre)
   ),   
   tar_target(
+    site_1, 
+    extraction(data_1, c("lat", "long"))
+  ),   
+  tar_target(
+    site_2, 
+    combinaison(site_1)
+  ),   
+  tar_target(
+    data_2, 
+    joindre_id(data_1, site_2)
+  ),   
+  tar_target(
     site, 
-    select
-  ),
-  tar_render(
-    name = "markdown.Rmd",
-    path = "C:/Users/lapin/Documents/School/Supériorité/8.Hiver 2025/BIO500 - Méthodes en écologie computationnelle/test"
+    quebec(site_2)
+  ),      
+  tar_target(
+    info_1, 
+    extraction(data_1, c("original_source", "creator", "title", "publisher", "intellectual_rights", "license", "owner"))
+  ),   
+  tar_target(
+    info, 
+    combinaison(info_1)
+  ),   
+  tar_target(
+    data_3, 
+    joindre_id(data_2, info)
+  ),   
+  tar_target(
+    obs_1, 
+    extraction(data_1, c("obs_unit", "obs_variable", "obs_value"))
+  ),   
+  tar_target(
+    obs, 
+    combinaison(obs_1)
+  ),   
+  tar_target(
+    data_4, 
+    joindre_id(data_3, obs)
+  ),   
+  tar_target(
+    main_1, 
+    extraction(data_1, c("observed_scientific_name", "year_obs", "day_obs", "time_obs", "dwc_event_date"))
+  ),   
+  tar_target(
+    main, 
+    combinaison(main_1)
+  ),   
+  tar_target(
+    data, 
+    joindre_id(data_4, main)
   ))
 
