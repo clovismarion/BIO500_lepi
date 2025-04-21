79a6746b8e1d697eb0ca0237b5d4f0574ca10f8f
VanHaiVu <lapinquimangeunebanane@gmail.com>
2025-04-21 12:05:45 -0400

    modif target

--- _targets.R
+++ _targets.R
@@ -20,7 +20,8 @@ source("fonctions/RSQLite_target.R")
 list(
   tar_target(
     liste_df, 
-    chargement(exclure = "taxonomie.csv", combiner = F)
+    chargement(exclure = "taxonomie.csv", 
+               combiner = F)
   ),   
   tar_target(
     donnees_propres, 
@@ -32,19 +33,27 @@ list(
   ),   
   tar_target(
     df_site, 
-    liste_donnees(data = donnees_brutes, cols = c("lat", "lon"), id_nom = "site")
+    liste_donnees(data = donnees_brutes, 
+                  cols = c("lat", "lon"), 
+                  id_nom = "site")
   ),   
   tar_target(
     df_info, 
-    liste_donnees(data = df_site$data_with_id, cols = c("original_source", "creator", "title", "publisher", "intellectual_rights", "license", "owner"), id_nom = "infos")
+    liste_donnees(data = df_site$data_with_id, 
+                  cols = c("original_source", "creator", "title", "publisher", "intellectual_rights", "license", "owner"), 
+                  id_nom = "infos")
   ),   
   tar_target(
     df_observation, 
-    liste_donnees(data = df_info$data_with_id, cols = c("obs_unit", "obs_variable", "obs_value"), id_nom = "obs")
+    liste_donnees(data = df_info$data_with_id, 
+                  cols = c("obs_unit", "obs_variable", "obs_value"), 
+                  id_nom = "obs")
   ),   
   tar_target(
     df_main, 
-    liste_donnees(data = df_observation$data_with_id, cols = c("observed_scientific_name", "year_obs", "day_obs", "time_obs", "dwc_event_date"), id_nom = "principal")
+    liste_donnees(data = df_observation$data_with_id, 
+                  cols = c("observed_scientific_name", "year_obs", "day_obs", "time_obs", "dwc_event_date"), 
+                  id_nom = "principal")
   ),   
   tar_target(
     df_site_qc, 
